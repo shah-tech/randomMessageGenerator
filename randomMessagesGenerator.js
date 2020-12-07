ace8d40c6e0af45d0edf651a5590c3c20f58336e
shahjan <shahjanali@yahoo.com>
2020-12-07 14:19:32 +0000

    first commit

---
+++ randomMessagesGenerator.js
@@ -0,0 +1,5 @@
+let namesArray = ['james','billy','adam','jake','tariq','moh','shah','ali','raza'];
+
+let specialPowers = ['Sonic Boom','EMP Whirlpool','FireBurn','Mind Control','Full stack Hack'];
+
+console.log('hello world');
